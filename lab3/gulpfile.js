83e18a75946455ed874f279d6d9f3580da18c4c7
Mark <40058900+mrzavgorodniy@users.noreply.github.com>
2020-09-12 17:21:54 +0300

    Add files via upload

---
+++ lab3/gulpfile.js
@@ -0,0 +1,14 @@
+﻿var gulp = require('gulp'),
+	jade = require('gulp-jade');
+
+//В командной строке набрать gulp mytask ля запуска задачи	
+gulp.task('mytask', function() {
+	
+  var variable = {};
+ 
+  return gulp.src('./src/*.jade')
+    .pipe(jade({
+      locals: variable
+    }))
+    .pipe(gulp.dest('./dist/'))
+});
